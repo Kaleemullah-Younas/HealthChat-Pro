3f89339557b1ecf1ee016127c94b220fef55d85f
Kaleemullah-Younas <kaleemullahyouus123@gmail.com>
2025-09-02 23:39:21 +0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='HealthChat-Pro',
+    version='0.1',
+    author='kaleem',
+    packages=find_packages(),
+    install_requires=[]
+)
